133d40a93bd13cc3d1325a4a534432e565a04fbb
janinelps <janinelpss@gmail.com>
2022-01-23 17:57:58 -0300

    Implementa useState, botão de busca

--- React-Portifolio-GitHub/src/App.js
+++ React-Portifolio-GitHub/src/App.js
@@ -3,20 +3,25 @@ import { Summary } from './components/summary/index'
 import { Repository } from './components/repository/index'
 import styles from './components/styles.module.css'
 import { useEffect, useState } from 'react';
-import {mapToRepoObject} from './data/data-utils'
+import { mapToRepoObject } from './data/data-utils'
 function App() {
   const [listaRepositoriosState, setRepositorios] = useState([])
   const [idSelecionado] = useState(2)
+  const [nomeUsuario, setNomeUsuario] = useState('')
 
   useEffect(() => {
     fetch("https://api.github.com/users/janinelps/repos")
-        .then((resposta) => resposta.json())
-        .then((resultado) => {
-            const resultadoMapeado = mapToRepoObject(resultado);
-            setRepositorios(resultadoMapeado);
-        });
-      }, []);
+      .then((resposta) => resposta.json())
+      .then((resultado) => {
+        const resultadoMapeado = mapToRepoObject(resultado);
+        setRepositorios(resultadoMapeado);
+      });
+  }, []);
 
+  const handleNomeUsuario = (value) => setNomeUsuario(value);
+  const handleBuscar = () => {
+    console.log(nomeUsuario)
+  }
   return (
     <div className='container'>
       <div className="App">
@@ -24,9 +29,10 @@ function App() {
           Meu portfólio Github!
         </h1>
       </div>
-
       <div className='grid'>
         <Summary img={'https://github.com/janinelps.png'} nome={'NOME'} />
+        <input type="text" onChange={(event) => handleNomeUsuario(event.target.value)} value={nomeUsuario} />
+        <button type='button' onClick={() => handleBuscar()}>Buscar</button>
 
         {listaRepositoriosState.map(evento => {
           return (
