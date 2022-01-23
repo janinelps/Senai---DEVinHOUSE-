0172979fa9762c85566dd9164efa3c545b6ec91e
janinelps <janinelpss@gmail.com>
2022-01-23 18:00:53 -0300

    Refatora useEffect finalização do projeto e desafio

--- React-Portifolio-GitHub/src/App.js
+++ React-Portifolio-GitHub/src/App.js
@@ -2,25 +2,35 @@ import './App.css';
 import { Summary } from './components/summary/index'
 import { Repository } from './components/repository/index'
 import styles from './components/styles.module.css'
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { mapToRepoObject } from './data/data-utils'
 function App() {
   const [listaRepositoriosState, setRepositorios] = useState([])
-  const [idSelecionado] = useState(2)
-  const [nomeUsuario, setNomeUsuario] = useState('')
+  const [idSelecionado, setIdSelecionado] = useState(0)
+  const [nomeUsuario, setNomeUsuario] = useState('janinelps')
 
-  useEffect(() => {
-    fetch("https://api.github.com/users/janinelps/repos")
-      .then((resposta) => resposta.json())
+  const handleNomeUsuario = (value) => setNomeUsuario(value);
+  const handleBuscar = () => fetchDadosDoUsuario()
+
+  const fetchDadosDoUsuario = () => {
+    fetch(`https://api.github.com/users/${nomeUsuario}/repos`)
+      .then((resposta) => {
+        if (!resposta.ok) {
+          return resposta.text().then(text => { throw new Error(text) })
+        }
+        return resposta.json()
+      }).catch(error => {
+        return [{ id: 0, full_name: 'ERRO AO CONECTAR NO REPOSITÓRIO', description: JSON.parse(error.message).message }]
+      })
       .then((resultado) => {
         const resultadoMapeado = mapToRepoObject(resultado);
         setRepositorios(resultadoMapeado);
-      });
-  }, []);
+      })
+  }
 
-  const handleNomeUsuario = (value) => setNomeUsuario(value);
-  const handleBuscar = () => {
-    console.log(nomeUsuario)
+  const selecionarProjeto = (id) => {
+    console.log(id)
+    setIdSelecionado(id)
   }
   return (
     <div className='container'>
@@ -30,15 +40,16 @@ function App() {
         </h1>
       </div>
       <div className='grid'>
-        <Summary img={'https://github.com/janinelps.png'} nome={'NOME'} />
+        <Summary img={`https://github.com/${nomeUsuario}.png`} nome={nomeUsuario} />
         <input type="text" onChange={(event) => handleNomeUsuario(event.target.value)} value={nomeUsuario} />
         <button type='button' onClick={() => handleBuscar()}>Buscar</button>
-
-        {listaRepositoriosState.map(evento => {
-          return (
-            <Repository key={evento.id} className={styles.repository} titulo={`${evento.id} - ${evento.titulo}`} descricao={evento.descricao} destacar={evento.id === idSelecionado} />
-          )
-        })}
+        <section className={styles.repository} >
+            {listaRepositoriosState.map(evento => {
+              return (
+                <Repository selecionarProjeto={selecionarProjeto} key={evento.id} className={styles.repository} id={evento.id} titulo={evento.titulo} descricao={evento.descricao} destacar={evento.id === idSelecionado} />
+              )
+            })}
+        </section >
       </div >
     </div >
   );
