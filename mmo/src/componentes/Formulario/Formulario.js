abb723eb67e8551832f2300e2115ed47162436e0
janinelps <janinelpss@gmail.com>
2022-01-31 19:50:58 -0300

    Implementa salvar dados do formulario no localStorage

--- mmo/src/componentes/Formulario/Formulario.js
+++ mmo/src/componentes/Formulario/Formulario.js
@@ -3,41 +3,38 @@ import { Field, Form, Formik } from "formik"
 
 export const Formulario = () => {
 
-
     const nomeLocalStorage = "comentario";
-    const arrayLocalStorage = (() => {
-        localStorage.getItem(nomeLocalStorage);
-        arrayLocalStorage ? JSON.parse(arrayLocalStorage) : []  
-})
 
-const handleSubmit = (values, { setSubmitting }) => {
-    setTimeout(() => {
-        localStorage.setItem(nomeLocalStorage, JSON.stringify(values, null, 2))
+    const listaStorageTemp = localStorage.getItem(nomeLocalStorage)
+    let listaStorage = listaStorageTemp ? JSON.parse(listaStorageTemp) : []
+
+    const handleSubmit = (values, { setSubmitting }) => {
+        listaStorage.push(values)
+        localStorage.setItem(nomeLocalStorage, JSON.stringify(listaStorage))
         setSubmitting(false)
-    }, 1000);
-}
+    }
 
-return (
-    <>
-        <Formik initialValues={{ text: '', email: '', comentario: '' }} onSubmit={handleSubmit}>
+    return (
+        <>
+            <Formik initialValues={{ nome: '', email: '', comentario: '' }} onSubmit={handleSubmit}>
 
-            {({ isSubmitting, resetForm }) => (
-                < Form >
-                    <h3>Comentários</h3>
-                    <div>
-                        <Field name='text' type='text' id='name' required placeholder='Nome Completo' />
-                        <Field name='email' type='email' id='email' required placeholder='E-mail' />
-                    </div>
-                    <div>
-                        <Field component="textarea" rows="10" required name='comentario' placeholder='Deixe seu comentario' />
-                    </div>
+                {({ isSubmitting, resetForm }) => (
+                    < Form >
+                        <h3>Comentários</h3>
+                        <div>
+                            <Field name='nome' type='text' required placeholder='Nome Completo' />
+                            <Field name='email' type='email' required placeholder='E-mail' />
+                        </div>
+                        <div>
+                            <Field component="textarea" rows="10" required name='comentario' placeholder='Deixe seu comentario' />
+                        </div>
 
-                    <button type="submit" disabled={isSubmitting}>ENVIAR</button>
-                    <button type="button" disabled={isSubmitting} onClick={resetForm} >LIMPAR</button>
+                        <button type="submit" disabled={isSubmitting}>ENVIAR</button>
+                        <button type="button" disabled={isSubmitting} onClick={resetForm} >LIMPAR</button>
 
-                </Form>
-            )}
-        </Formik>
-    </>
-)
+                    </Form>
+                )}
+            </Formik>
+        </>
+    )
 }
